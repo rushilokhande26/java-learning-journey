015182ffc8edc95395bab260bde4d25f8e73133e
Rushikesh Lokhande <lokhanderushikesh470@gmail.com>
2026-03-14 00:54:10 +0530

    Added Day01 HelloWorld Java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
+// Day 1 - First Java Program
+// Author: Rushikesh Lokhande
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, Java World!");
+        System.out.println("Hello Java World");
 
     }
 
