2cddff281c4fd608a401f54ca2aaec7f08fa6a53
Rushikesh Lokhande <lokhanderushikesh470@gmail.com>
2026-03-22 16:49:33 +0530

    Day 3 update

---
+++ Day03-DataTypes/datatypes.java
@@ -0,0 +1,19 @@
+//package com.fullStackdeveloper;
+public class datatypes {
+public static void main(String[]args) {
+double marks = 90.33;
+String name = "Rushi";
+char grade = 'a';
+String address = "pandharpur";
+long Mobile_No = 929898989;
+int age = 34;
+String Parent_Mobile_no = "989847757";
+System.out.println("student marks :"+marks);
+System.out.println("student name :"+name);
+System.out.println("student grade :"+grade);
+System.out.println("student address :"+address);
+System.out.println("student mobile no:"+Mobile_No);
+System.out.println("student age :"+age);
+System.out.println("Parent_Mobile_no :"+Parent_Mobile_no);
+}
+}
