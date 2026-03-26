0387b3635cf67d15a07a01e2b3f04e93e707474b
Rushikesh Lokhande <lokhanderushikesh470@gmail.com>
2026-03-26 14:30:25 +0530

    Added Day 4 - Conditional Statements

---
+++ Day04-Conditional-Statements/ConditionalStatement.java
@@ -0,0 +1,52 @@
+
+// IfExample.java
+
+// class IfExample {
+//     public static void main(String[] args) {
+//         int age = 20;
+//         if(age >= 18){
+//             System.out.println("Eligible to vote");
+//         }
+//     }
+// }
+
+// IfElseExample.java
+// class IfElseExample {
+//     public static void main(String[] args) {
+//         int age = 16;
+//         if(age >= 18){
+//             System.out.println("Adult");
+//         } else {
+//             System.out.println("Minor");
+//         }
+//     }
+// }
+
+// ElseIfExample.java
+// class ElseIfExample {
+//     public static void main(String[] args) {
+//         int marks = 75;
+
+//         if(marks >= 80){
+//             System.out.println("Excellent");
+//         } else if(marks >= 50){
+//             System.out.println("Pass");
+//         } else {
+//             System.out.println("Fail");
+//         }
+//     }
+// }
+
+// NestedIfExample.java
+// class NestedIfExample {
+//     public static void main(String[] args) {
+//         int age = 20;
+//         boolean hasID = true;
+
+//         if(age >= 18){
+//             if(hasID){
+//                 System.out.println("Allowed");
+//             }
+//         }
+//     }
+// }
